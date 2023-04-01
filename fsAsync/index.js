d8cbe624530e9d12cd5a87802bfadf4159ecfa99
Mondeep Kalita <epicdeep09@gmail.com>
2023-04-01 16:49:05 +0530

    All Async file operations!
    TT - V9

--- fsAsync/index.js
+++ fsAsync/index.js
@@ -44,7 +44,57 @@ const fs = require("fs");
 
 // OR
 
-fs.readFile('asyncFile.txt', "utf-8" ,(e, data) => {
+// fs.readFile('asyncFile.txt', "utf-8" ,(e, data) => {
+//     console.log(data)
+//     console.log(e)
+// })
+
+
+// ||||||   V9   |||||
+
+// Create a folder
+// fs.mkdir('../asyncFol', () => {
+//     console.log('Folder created')
+// })
+
+// Create a file in it
+// fs.writeFile('../asyncFol/bio.txt', 'New file', () => {
+//     console.log('File created')
+// })
+
+// append data
+// fs.appendFile('../asyncFol/bio.txt', '. New texts!', () => {
+//     console.log('Data added')
+// })
+
+// Read the data
+fs.readFile('../asyncFol/newBiso.txt', 'utf-8', (e, data) => {
     console.log(data)
-    console.log(e)
+    console.log('File read successful')
+    console.log(e)                              // This is will give the detalied error description
+
 })
+
+// Rename file
+// fs.rename('../asyncFol/bio.txt', '../asyncFol/newBio.txt', () => {
+//     console.log('Renamed!')
+// })
+
+// Delete file & folder
+// Let's create two new
+
+// fs.mkdir('./af', () => {
+//     console.log('Folder done')
+// })
+
+// fs.writeFile('./bio.txt', 'Gonna delete this', () => {
+//     console.log('Delete this')
+// })
+
+// Now remove these two
+// fs.rmdir('./af', () => {
+//     console.log('Deleted')
+// })
+// fs.unlink('./bio.txt', () => {
+//     console.log('Deleted')
+// })
