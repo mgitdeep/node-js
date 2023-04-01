6768e9f4113139efb09cd92d659688f0b85937a0
Mondeep Kalita <epicdeep09@gmail.com>
2023-04-01 17:51:37 +0530

    PATH module
    TT - V11

---
+++ PATH-module/index.js
@@ -0,0 +1,9 @@
+
+const path = require('path');
+
+console.log(path)
+console.log(path.dirname("C:/Users/user/Desktop/LIFE PROJECTS/Project Documentation/Special Course Notes/Front End Development/ALL REPOS/node-js/PATH-module/index.js"))
+console.log(path.extname("C:/Users/user/Desktop/LIFE PROJECTS/Project Documentation/Special Course Notes/Front End Development/ALL REPOS/node-js/PATH-module/index.js"))
+console.log(path.basename("C:/Users/user/Desktop/LIFE PROJECTS/Project Documentation/Special Course Notes/Front End Development/ALL REPOS/node-js/PATH-module/index.js"))
+// Change "\" to "/" in above paths after copying the path by right clicking 
+console.log(path.parse("C:/Users/user/Desktop/LIFE PROJECTS/Project Documentation/Special Course Notes/Front End Development/ALL REPOS/node-js/PATH-module/index.js").name)
