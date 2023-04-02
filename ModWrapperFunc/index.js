af320265236336b3a20faac371c327d82a151701
Mondeep Kalita <epicdeep09@gmail.com>
2023-04-02 10:53:07 +0530

    Module Wrapper Function & IIFE

---
+++ ModWrapperFunc/index.js
@@ -0,0 +1,21 @@
+
+// (function (exports, require, module, __filename, __dirname) {
+
+//     const a = require('fs')
+//     const name = 'Mondeep'
+//     module.exports = {sdghkasgdj}
+//     console.log(a, name)
+// })
+
+
+// |||   IIFE   |||
+(function () {
+    const a = "Mondeep's empire is near"
+    console.log(a)
+})()
+
+
+// some extra concept
+
+console.log(__dirname)
+console.log(__filename)
