bdf4052e9068d02971eb8bf55463de89d852d845
Mondeep Kalita <epicdeep09@gmail.com>
2023-04-02 10:04:23 +0530

    Installed global NODEMON

--- NPM-modules/index.js
+++ NPM-modules/index.js
@@ -5,5 +5,5 @@ const validator = require('validator')
 
 console.log(chalk.blue.inverse('Hello world!'));
 
-const mail = validator.isEmail('mk@123.w')
+const mail = validator.isEmail('mk@123.com')
 console.log(mail ? chalk.green.inverse(mail) : chalk.red.inverse(mail))
